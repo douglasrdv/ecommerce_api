4d5dc0d50ab43fa182a21cd0278b8c51e878c43d
douglasrdv <douglasrdv@proton.me>
2024-01-27 14:07:26 -0300

    Add remove from cart, view cart and checkout route

--- app.py
+++ app.py
@@ -1,14 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
-from flask_login import (
-    UserMixin,
-    login_user,
-    LoginManager,
-    login_required,
-    logout_user,
-    current_user,
-)
+from flask_login import (UserMixin, login_user, LoginManager, login_required, logout_user, current_user,)
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "minha_chave_123"
@@ -153,6 +146,50 @@ def add_to_cart(product_id):
         db.session.commit()
         return jsonify({"message": "Item added to the cart successfully"})
     return jsonify({"message": "Failed to add item to the cart"}), 400
+
+
+@app.route("/api/cart/remove/<int:product_id>", methods=["DELETE"])
+@login_required
+def remove_from_cart(product_id):
+    cart_item = CartItem.query.filter_by(
+        user_id=current_user.id, product_id=product_id
+    ).first()
+    if cart_item:
+        db.session.delete(cart_item)
+        db.session.commit()
+        return jsonify({"message": "Item removed from the cart successfully"})
+    return jsonify({"message": "Failed to remove item from the cart"}), 400
+
+
+@app.route("/api/cart", methods=["GET"])
+@login_required
+def view_cart():
+    user = User.query.get(int(current_user.id))
+    cart_items = user.cart
+    cart_content = []
+    for cart_item in cart_items:
+        product = Product.query.get(cart_item.product_id)  # Pode ser feito uma melhoria de performance
+        cart_content.append(
+            {
+                "id": cart_item.id,
+                "user_id": cart_item.user_id,
+                "product_id": cart_item.product_id,
+                "product_name": product.name,
+                "product_price": product.price,
+            }
+        )
+    return jsonify(cart_content)
+
+
+@app.route("/api/cart/checkout", methods=["POST"])
+@login_required
+def checkout():
+    user = User.query.get(int(current_user.id))
+    cart_items = user.cart
+    for cart_item in cart_items:
+        db.session.delete(cart_item)
+    db.session.commit()
+    return jsonify({"message": "Checkout successful. Cart has been cleared."})
 
 
 if __name__ == "__main__":
